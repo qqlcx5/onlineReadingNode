900b0c78271a0283e76de6ce684efe53e0d67f62
qqlcx5 <893917884@qq.com>
2020-05-11 11:57:05 +0800

    md5加密

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = require('./router');
+const app = express();
+const fs = require('fs')
+const https = require('https')
+// 控制台获取body信息
+const bodyParser = require('body-parser')
+const cors = require('cors')
+// https签名
+const privateKey = fs.readFileSync('./https/3876613_liaocx.design.key', 'utf8')
+const certificate = fs.readFileSync('./https/3876613_liaocx.design.pem', 'utf8')
+const credentials = { key: privateKey, cert: certificate }
+const httpsServer = https.createServer(credentials, app)
+
+const SSLPORT = 18082
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// 跨域
+app.use(cors())
+
+app.use('/', router);
+const server = app.listen(5000, () => {
+  const { address, port } = server.address();
+  console.log('Http Server is running on http://%s:%s', address, port);
+});
+
+
+httpsServer.listen(SSLPORT, function() {
+  console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT)
+})
---
+++ services/user.js
@@ -0,0 +1,11 @@
+
+const { querySql } = require('../db')
+
+function login(username, password) {
+  const sql = `select * from admin_user where username='${username}' and password='${password}'`
+  return querySql(sql)
+}
+
+module.exports = {
+  login
+}
