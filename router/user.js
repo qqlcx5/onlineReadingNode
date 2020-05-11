f162076301fa81c9258c3c873145141b3c860390
qqlcx5 <893917884@qq.com>
2020-05-11 14:47:59 +0800

    JWT Token

--- router/user.js
+++ router/user.js
@@ -1,23 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const Result = require('../models/Result')
-const { login } = require('../services/user')
-const { md5 } = require('../utils/index')
-const { PWD_SALT } = require('../utils/constant')
+const Result = require('../models/Result');
+const { login } = require('../services/user');
+const { md5 } = require('../utils/index');
+const { PWD_SALT } = require('../utils/constant');
+const { body, validationResult } = require('express-validator');
+const boom = require('boom');
+const jwt = require('jsonwebtoken');
+const { PRIVATE_KEY, JWT_EXPIRED } = require('../utils/constant');
 
-
-router.post('/login', function(req, res, next) {
-  let {username,password} = req.body
-  password = md5(`${password}${PWD_SALT}`)
-  
-  login(username, password).then(user => {
-    if (!user || user.length === 0) {
-      new Result('登录失败').fail(res)
+router.post(
+  '/login',
+  [
+    body('username').isString().withMessage('username类型不正确'),
+    body('password').isString().withMessage('password类型不正确'),
+  ],
+  function (req, res, next) {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+      const [{ msg }] = err.errors;
+      next(boom.badRequest(msg));
     } else {
-      new Result('登录成功').success(res)
+      const username = req.body.username;
+      const password = md5(`${req.body.password}${PWD_SALT}`);
+
+      login(username, password).then((user) => {
+        if (!user || user.length === 0) {
+          new Result('登录失败').fail(res);
+        } else {
+          const token = jwt.sign(
+            { username },
+            PRIVATE_KEY,
+            { expiresIn: JWT_EXPIRED }
+          )
+          new Result({token},'登录成功').success(res);
+        }
+      });
     }
-  })
-})
+  }
+);
 
 router.get('/info', function (req, res, next) {
   res.json('user info...');
